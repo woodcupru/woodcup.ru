4fd84323a62d5adcb26e1749aaf21dca3da647c0
Nikolay Semenov <nikolaysemenov.int@gmail.com>
2023-03-07 20:53:07 +0300

    12

---
+++ js/main.js
@@ -0,0 +1,88 @@
+$.fn.exists = function(callback) {
+  var args = [].slice.call(arguments, 1);
+  if (this.length) {
+    callback.call(this, args);
+  }
+  return this;
+};
+
+
+$(document).ready(function() {
+    
+$('#top_nav').exists(function () {
+    
+$(window).scroll(function() {
+  
+  // selectors
+  var $window = $(window),
+      $body = $('#top_nav'),
+      $panel = $('.spyblock');
+
+  var scroll = $window.scrollTop() + ($window.height() / 3);
+ 
+  $panel.each(function () {
+    var $this = $(this);
+    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+
+     $body.removeClass(function (index, css) {
+        return (css.match (/(^|\s)bg-\S+/g) || []).join(' ');
+      });
+      $body.addClass('bg-' + $(this).data('color'));
+    }
+  });    
+  
+}).scroll();
+    
+});    
+    
+
+$('.image-popup').magnificPopup({
+	type: 'image',
+	closeOnContentClick: true,
+	mainClass: 'mfp-img-mobile',
+	image: {
+		verticalFit: true
+	}	
+});
+ 
+ 
+$('#switch-lang').css({'pointer-events':'none',
+   'cursor':'default'}).attr('disabled','disabled');
+
+  function langButtonListen() {
+    $('#switch-lang').click(function (event) {
+      event.preventDefault();
+      $('[lang="ru"]').toggle();
+      $('[lang="en"]').toggle();
+      // Switch cookie stored language.
+      if ($.cookie('lang') === 'en') {
+        $.cookie('lang', 'zh', { expires: 7 });
+      } else {
+        $.cookie('lang', 'en', { expires: 7 });
+      }
+    });
+    // Enable lang switching button.
+    $('#switch-lang').css({'pointer-events':'auto',
+     'cursor':'pointer'}).removeAttr('disabled');
+  }
+
+  // Check if language cookie already exists.
+  if ($.cookie('lang')) {
+    var lang = $.cookie('lang');
+    if (lang === 'en') {
+      $('[lang="ru"]').hide();
+      langButtonListen();
+    } else {
+      $('[lang="en"]').hide();
+      langButtonListen();
+    }
+  } else {
+        
+      $('[lang="ru"]').hide();
+      $.cookie('lang', 'en', { expires: 7 });
+      langButtonListen();
+  }
+
+    
+    
+});
